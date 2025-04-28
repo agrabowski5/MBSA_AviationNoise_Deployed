0454d812f262b2d4867b6b86e55c4e0b85365ecb
mlogrono <mlogrono@mit.edu>
2025-04-28 13:49:35 +0200

    Fixing bug in the title slide scrolling

--- src/slides/Slide1.svelte
+++ src/slides/Slide1.svelte
@@ -1,16 +1,10 @@
-<script>
-    export let active = false;
+<div class="slide">
+    <h1>When the Skies Get Loud</h1>
+    <h2>Health and Housing Impacts of Aviation Noise</h2>
+    <p>- Kailey Bridgeman, Andrew Grabowski, Marcos Logrono, Shreya Sharma  -</p>
+    <div class="scroll-indicator"></div>
+</div>
 
-</script>
-
-{#if (active)}
-    <div class="slide">
-        <h1>When the Skies Get Loud</h1>
-        <h2>Health and Housing Impacts of Aviation Noise</h2>
-        <p>- Kailey Bridgeman, Andrew Grabowski, Marcos Logrono, Shreya Sharma  -</p>
-        <div class="scroll-indicator"></div>
-    </div>
-{/if}
 
 <style>
     @import url("$lib/slide.css");
